fbd02e9235854b0640bf7734920a7cfe00763c83
Josh Barrow <josh@jukaela.com>
2012-12-22 13:33:51 -0600

    Add ability to view entire quote and image in new activity

--- src/com/example/quotereader/QuoteDetail.java
+++ src/com/example/quotereader/QuoteDetail.java
@@ -1,13 +1,34 @@
 package com.example.quotereader;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class QuoteDetail extends Activity {
 
+	private ImageView mImageView;
+	private TextView mQuote;
+	private int mPosition;
+	private DataSource mDataSource;
+	 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.quote_detail);
+		
+		Intent i = getIntent();
+	
+		mPosition = i.getIntExtra("position", 0);
+		
+		mDataSource = new DataSource();
+		
+		mImageView = (ImageView)findViewById(R.detail.image);
+		
+		mQuote = (TextView)findViewById(R.detail.quote);
+		
+		mImageView.setImageResource(mDataSource.getmPhotoHdPool().get(mPosition));
+		mQuote.setText(getResources().getString(mDataSource.getmQuotePool().get(mPosition)));
 	}
 }
